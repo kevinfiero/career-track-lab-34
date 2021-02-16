f0f7eae7453b7056bb34cb2e87a24cf34f7fe071
kevinfiero <kjf46@cornell.edu>
2021-02-16 15:03:02 -0800

    passing tests

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
@@ -8,4 +8,77 @@ describe('App component', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('test for color selection', async() => {
+    render(<App />);
+
+    const colorInput = await screen.findByAltText('Color Picker');
+
+    fireEvent.change(colorInput, {
+      target: {
+        value: '#00FF00'
+      }
+    });
+
+    const display = await screen.findByTestId('Color Result');
+    console.log(display.style.backgroundColor);
+    
+    expect(display).toHaveStyle({
+      backgroundColor: '#00FF00'
+
+    });
+  });
+
+  it('test for undo selection', async() => {
+    render(<App />);
+
+    const undoInput = await screen.findByText('undo');
+    const colorInput = await screen.findByAltText('Color Picker');
+
+    fireEvent.change(colorInput, {
+      target: {
+        value: '#00FF00'
+      }
+    });
+
+    fireEvent.click(undoInput);
+
+    const display = await screen.findByTestId('Color Result');
+    console.log(display.style.backgroundColor);
+
+    expect(display).toHaveStyle({
+      backgroundColor: '#FF0000'
+
+    });
+  });
+
+  it('test for redo selection', async() => {
+    render(<App />);
+
+    const undoInput = await screen.findByText('undo');
+    const redoInput = await screen.findByText('redo');
+    const colorInput = await screen.findByAltText('Color Picker');
+
+    fireEvent.change(colorInput, {
+      target: {
+        value: '#00FF00'
+      }
+    });
+
+    fireEvent.click(undoInput);
+    fireEvent.click(redoInput);
+
+    const display = await screen.findByTestId('Color Result');
+
+    console.log(display.style.backgroundColor);
+
+    expect(display).toHaveStyle({
+      backgroundColor: '#00FF00'
+    });
+  });
+
+
+
+
+
 });
